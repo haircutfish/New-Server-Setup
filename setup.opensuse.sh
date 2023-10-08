6a264a0b093895c7dc46f9cc833e6dd0e6567239
HaircutFish <68355453+haircutfish@users.noreply.github.com>
2023-10-08 16:49:54 -0400

    Update setup.opensuse.sh

--- setup.opensuse.sh
+++ setup.opensuse.sh
@@ -9,14 +9,18 @@
 # here is a Gist with the all Timezones:
 # https://gist.github.com/alejzeis/ad5827eb14b5c22109ba652a1a267af5
 # Place the Timezone name between the single qoutes.
-username=
-password=
-lm_name=
-timezone=''
+USERNAME=
+PASSWORD=
+LM_NAME=
+TIMEZONE=
+
+if [ -z "$USERNAME" ] || [ -z "$PASSWORD" ] || [ -z "$LM_NAME" ] || [ -z "$TIMEZONE" ]; then
+    echo "One or more required variables are empty. Exiting the script."
+    exit 1
+fi
 
 # Prompts you will receive after running this script:
 # install yast2-online-update-configuration
-# fail2ban
 # firewalld
 
 
@@ -29,17 +33,17 @@ zypper install yast2-online-update-configuration
 sed -i 's/AOU_PATCH_CATEGORIES=""/AOU_PATCH_CATEGORIES="security"/g' /etc/sysconfig/automatic_online_update
 
 # Set timezone
-timedatectl set-timezone "$timezone"
+timedatectl set-timezone "$TIMEZONE"
 
 # Setting hostname/machine name
-hostnamectl set-hostname "$lm_name"
+hostnamectl set-hostname "$LM_NAME"
 
 # creating a limited user
-useradd -m -s /bin/bash "$username"
-echo "$username":"$password" | chpasswd
+useradd -m -s /bin/bash "$USERNAME"
+echo "$USERNAME":"$PASSWORD" | chpasswd -c SHA256
 
 # If you want the user as a Sudoer, uncomment out the next line
-#usermod -aG wheel "$username"
+#usermod -aG wheel "$USERNAME"
 
 # Install Firewalld and configure it
 zypper install firewalld
